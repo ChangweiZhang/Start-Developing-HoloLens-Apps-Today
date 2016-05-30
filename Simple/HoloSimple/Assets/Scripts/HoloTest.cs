c1b422738a814fb0d9cc42b39986a92a1c9b5908
Changwei <z18205051832@hotmail.com>
2016-05-30 15:18:13 +0800

    upload documents and codes

---
+++ Simple/HoloSimple/Assets/Scripts/HoloTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoloTest : MonoBehaviour
+{
+
+    void OnSelect()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0, 255) / 255f, Random.Range(0, 255) / 255f, Random.Range(0, 255) / 255f);
+    }
+}
